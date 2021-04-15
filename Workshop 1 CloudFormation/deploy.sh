784670c13b9f8c520b935f1ba75c59ec6aa8b3d5
laimonassutkus <laimonas.sutkus@gmail.com>
2021-04-15 10:48:56 +0300

    Deployment fixes.

--- Workshop 1 CloudFormation/deploy.sh
+++ Workshop 1 CloudFormation/deploy.sh
@@ -1,2 +1,5 @@
 path="$1"
-aws cloudformation deploy --template-file "$path" --stack-name WorkshopStack
+aws cloudformation deploy \
+  --template-file "$path" \
+  --stack-name WorkshopStack \
+  --capabilities CAPABILITY_IAM
